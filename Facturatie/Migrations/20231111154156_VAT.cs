53444063c67476aab65162469b984587f4f501ab
arneverbiest <Arne.verbiest@gmail.com>
2023-11-15 02:51:40 +0100

    Final Update Client

---
+++ Facturatie/Migrations/20231111154156_VAT.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Facturatie.Migrations
+{
+    /// <inheritdoc />
+    public partial class VAT : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<double>(
+                name: "VAT",
+                table: "Invoice",
+                type: "float",
+                nullable: false,
+                defaultValue: 0.0);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "VAT",
+                table: "Invoice");
+        }
+    }
+}
